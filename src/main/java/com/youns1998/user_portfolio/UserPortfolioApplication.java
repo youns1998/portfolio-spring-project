594970395d0e161196dadeccb5f3ea9c90a3ae2b
Choi <Choi@DESKTOP-UN8BP8O>
2024-11-18 15:02:08 +0900

    first commit

---
+++ src/main/java/com/youns1998/user_portfolio/UserPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.youns1998.user_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youns1998/user_portfolio/UserPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youns1998.user_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
